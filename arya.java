c93cfe283e7bce1f773726892bc221be6b45e0df
testerpoojaa <147801711+testerpoojaa@users.noreply.github.com>
2025-02-10 12:53:35 +0530

    newoneee

---
+++ arya.java
@@ -0,0 +1,14 @@
+package new1;
+
+public class arya1 {
+public int a=100;
+		public void show() {
+			System.out.println(a);
+		}
+	public static void main(String[] args) {
+		arya1 obj=new arya1();
+		obj.show();
+
+	}
+
+}
